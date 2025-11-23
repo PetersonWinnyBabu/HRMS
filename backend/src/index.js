6dea15e3e6b75595eb8df9f1633870c9172c8226
Peterson Winny Babu <88373271+PetersonWinnyBabu@users.noreply.github.com>
2025-11-23 21:12:35 +0530

    add updated urls

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 //Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://hrms-orcin-seven.vercel.app/",
     credentials: true,
   })
 );
